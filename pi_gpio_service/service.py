369778a058667c7f98a84293c1b86de5c394ec67
beepscore <support@beepscore.com>
2017-10-13 22:01:33 -0700

    Uncomment code and revise it. TODO: test on a raspberry pi

--- pi_gpio_service/service.py
+++ pi_gpio_service/service.py
@@ -4,20 +4,28 @@
 """
 
 from flask import Flask, jsonify, request
-# import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 
 # app is a flask object
 app = Flask(__name__)
 
 # https://raspberrypi.stackexchange.com/questions/12966/what-is-the-difference-between-board-and-bcm-for-gpio-pin-numbering#12967
-# GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BCM)
 
 
 # use pins dictionary to keep info "dry"
-pins = {}
+pins = {
+    '23': {'name': 'IN_23', 'pin_direction': 'input'},
+    '24': {'name': 'OUT_24', 'pin_direction': 'output'},
+    '25': {'name': 'OUT_25', 'pin_direction': 'output'}
+}
 
 
 def input_pins(pins):
+    """
+    :param pins: dictionary of all pins, input and output
+    :return: dictionary of pins configured as input
+    """
     inputs = {}
     for key in pins.keys():
         if pins[key]['pin_direction'] == 'input':
@@ -27,6 +35,10 @@ def input_pins(pins):
 
 
 def output_pins(pins):
+    """
+    :param pins: dictionary of all pins, input and output
+    :return: dictionary of pins configured as output
+    """
     outputs = {}
     for key in pins.keys():
         if pins[key]['pin_direction'] == 'output':
@@ -39,55 +51,66 @@ VALID_HIGH_VALUES = [1, '1', 'HIGH']
 VALID_LOW_VALUES = [0, '0', 'LOW']
 
 
-# def configure_pin_as_input(pin_number):
-#     GPIO.setup(pin_number, GPIO.IN)
+def configure_pin_as_input(pin_number):
+    GPIO.setup(pin_number, GPIO.IN)
 
 
-# def configure_pin_as_output(pin_number):
-#     GPIO.setup(pin_number, GPIO.OUT)
-#     GPIO.output(pin_number, GPIO.LOW)
+def configure_pin_as_output(pin_number):
+    GPIO.setup(pin_number, GPIO.OUT)
+    GPIO.output(pin_number, GPIO.LOW)
 
 
-# def configure_pins(pins):
-#     for pin in input_pins(pins):
-#         configure_pin_as_input(pin)
-# 
-#     for pin in output_pins(pins):
-#         configure_pin_as_output(pin)
+def configure_pins(pins):
+    for pin in input_pins(pins):
+        configure_pin_as_input(pin)
+
+    for pin in output_pins(pins):
+        configure_pin_as_output(pin)
 
 
-# def pin_status(pin_number_string):
-#     if pin_number in pins:
-#         value = GPIO.input(pin_number)
-#         data = {'pin_number': pin_number,
-#                 'pin_name': pins[pin_number],
-#                 'value': value,
-#                 'status': 'SUCCESS',
-#                 'error': None}
-#     else:
-#         data = {'status': 'ERROR',
-#                 'error': 'Invalid pin number.'}
-# 
-#     return data
+def pin_status(pin_number_string):
+    if pin_number_string in pins.keys():
+        pin_number = int(pin_number_string)
+
+        value = GPIO.input(pin_number)
+
+        pin = pins[pin_number_string]
+        data = {'pin_number': pin_number_string,
+                'pin_name': pin['name'],
+                'pin_direction': pin['pin_direction'],
+                'value': value,
+                'status': 'SUCCESS',
+                'error': None}
+    else:
+        data = {'status': 'ERROR',
+                'error': 'Invalid pin number.'}
+
+    return data
 
 
-# def pin_update(pin_number, value):
-#     if pin_number in pins.keys:
-#         GPIO.output(pin_number, value)
-#         new_value = GPIO.input(pin_number)
-#         data = {'status': 'SUCCESS',
-#                 'error': None,
-#                 'pin_number': pin_number,
-#                 'pin_name': pins[pin_number],
-#                 'new_value': new_value}
-#     else:
-#         data = {'status': 'ERROR',
-#                 'error': 'Invalid pin number or value.'}
-# 
-#     return data
+def pin_update(pin_number_string, value):
+    if pin_number_string in pins.keys():
+        pin_number = int(pin_number_string)
+
+        GPIO.output(pin_number, value)
+        new_value = GPIO.input(pin_number)
+
+        pin = pins[pin_number_string]
+        data = {'pin_number': pin_number_string,
+                'pin_name': pin['name'],
+                'pin_direction': pin['pin_direction'],
+                'new_value': new_value,
+                'status': 'SUCCESS',
+                'error': None}
+    else:
+        data = {'status': 'ERROR',
+                'error': 'Invalid pin number or value.'}
+
+    return data
 
 
-# configure_pins(pins)
+configure_pins(pins)
+
 
 # / is the website root, the entry point
 # http://127.0.0.1:5000
