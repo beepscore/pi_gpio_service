077b5fd0ed6e06029ffa69530f963260f3e9287c
beepscore <support@beepscore.com>
2017-10-14 11:36:50 -0700

    Add route set-all-output-high

--- pi_gpio_service/service.py
+++ pi_gpio_service/service.py
@@ -164,6 +164,16 @@ def gpio_status():
     return jsonify(data)
 
 
+@app.route("/api/v1/gpio/set-all-output-high/", methods=['POST'])
+def gpio_set_all_output_high():
+    data_list = []
+    for pin_number_string in sorted(output_pins(pins).keys()):
+        data_list.append(pin_update(pin_number_string, 1))
+
+    data = {'data': data_list}
+    return jsonify(data)
+
+
 if __name__ == '__main__':
     try:
         # '0.0.0.0' accessible to any device on the network
